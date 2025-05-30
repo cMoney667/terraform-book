d092291a8f28c8cdb79a333bcb749544f477ebe3
cMoney667 <ococncol@gmail.com>
2025-05-30 07:39:30 -0400

    Cluster is now working

--- main.tf
+++ main.tf
@@ -3,21 +3,14 @@ provider "aws" {
 }
 
 resource "aws_launch_template" "example" {
-  name          = "example-launch-template"
-  image_id      = "ami-0fb653ca2d3203ac1"
+  name_prefix = "terraform-example"
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups = [aws_security_group.instance.id]
-  }
-
-  user_data = base64encode(<<-EOF
-                  #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-             EOF
-  )
+  user_data = base64encode(templatefile("user-data.sh", {
+    server_port = var.server_port
+  }))
 
   lifecycle {
     create_before_destroy = true
@@ -29,7 +22,8 @@ resource "aws_autoscaling_group" "example" {
     id = aws_launch_template.example.id
     version = "$Latest"
   }
-  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  vpc_zone_identifier = data.aws_subnets.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -38,34 +32,23 @@ resource "aws_autoscaling_group" "example" {
   max_size = 10
 
   tag {
-    key                 = "Name"
-    value               = "terraform-example"
+    key = "Name"
+    value = "terraform-asg-example"
     propagate_at_launch = true
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port = var.server_port
-    to_port   = var.server_port
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
+  name = "terraform-asg-example"
   load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
+  subnets = data.aws_subnets.default.ids
   security_groups = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
+  port = 80
+  protocol = "HTTP"
 
   default_action {
     type = "fixed-response"
@@ -73,50 +56,31 @@ resource "aws_lb_listener" "http" {
     fixed_response {
       content_type = "text/plain"
       message_body = "404: page not found"
-      status_code  = 404
+      status_code = 404
     }
   }
 }
 
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
-  port     = var.server_port
+  name = "terraform-asg-example"
+  port = var.server_port
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 
   health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
     unhealthy_threshold = 2
   }
 }
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority     = 100
+  priority = 100
 
   condition {
     path_pattern {
@@ -125,10 +89,38 @@ resource "aws_lb_listener_rule" "asg" {
   }
 
   action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 data "aws_vpc" "default" {
   default = true
@@ -143,11 +135,11 @@ data "aws_subnets" "default" {
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
+  type = number
+  default = 8080
 }
 
 output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
+  value = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
