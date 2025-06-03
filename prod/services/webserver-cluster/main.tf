238db9b0aa15f45970c02c99b2502dd7433deffa
cMoney667 <ococncol@gmail.com>
2025-06-03 08:17:45 -0400

    working on making modules

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cmoney-terraform-up-and-running-state"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
+    key = "prod/services/webserver-cluster/terraform.tfstate"
     region = "us-east-2"
 
     # Replace this with your DynamoDB table name!
@@ -17,11 +17,11 @@ provider "aws" {
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webservers-stage"
+  cluster_name = "webservers-prod"
   db_remote_state_bucket = "cmoney-terraform-up-and-running-state"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
 
-  instance_type = "t2.micro"
+  instance_type = "m4.large"
   min_size = 2
-  max_size = 2
+  max_size = 10
 }
